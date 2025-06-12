ad84e1238816c9c479b096108f264418d0adf521
kiyodori <kiyodori@yahoo.co.jp>
2025-06-12 12:45:52 +0900

    Add casw 2

---
+++ case2/src/utils/math.js
@@ -0,0 +1,9 @@
+export function average(arr) {
+  if (arr.length === 0) return 0;
+  // バグ：最後の要素を集計していない
+  let sum = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
